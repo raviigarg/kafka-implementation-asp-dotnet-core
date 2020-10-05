ba955dcd08b341a9ff40876d1c15d665834db63f
Ravi Kant Garg <ravii.garg24@gmail.com>
2020-10-05 17:36:01 +0530

    Add basic producer

--- Source/KafkaProducer.ConsoleApp/KafkaProducer.ConsoleApp/Producer.cs
+++ Source/KafkaProducer.ConsoleApp/KafkaProducer.ConsoleApp/Producer.cs
@@ -1,38 +1,37 @@
 ﻿using Confluent.Kafka;
 using KafkaProducer.ConsoleApp.Helpers;
-using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace KafkaProducer.ConsoleApp
 {
-    public class Publisher
+    public class Producer
     {
 
-        private ProducerConfig _producerConfig;
-        public Publisher(IDictionary<string, string> config)
+        private readonly ProducerConfig _producerConfig;
+        public Producer(Dictionary<string, string> config)
         {
             _producerConfig = new ProducerConfig(config);
         }
-
-        public async Task<DeliveryResult<Null, TValue>> Publish<TValue>(TValue message, string topic)
+        
+        public async Task<DeliveryResult<Null, TValue>> Publish<TValue>(string topic, TValue message)
         {
             using var producer = new ProducerBuilder<Null, TValue>(_producerConfig)
                .SetValueSerializer(new JsonSerializer<TValue>())
                .Build();
-            var topicName = String.IsNullOrEmpty(topic) ? message.GetType().Name : topic;
+            var topicName = string.IsNullOrEmpty(topic) ? message.GetType().Name : topic;
             var deliveryResult = await producer.ProduceAsync(topicName, new Message<Null, TValue>() { Value = message });
             producer.Flush();
             producer?.Dispose();
             return deliveryResult;
         }
 
-        public async Task<DeliveryResult<TKey, TValue>> Publish<TKey, TValue>(TKey key, TValue message, string topic)
+        public async Task<DeliveryResult<TKey, TValue>> Publish<TKey, TValue>(string topic, TKey key, TValue message)
         {
             using var producer = new ProducerBuilder<TKey, TValue>(_producerConfig)
                .SetValueSerializer(new JsonSerializer<TValue>())
                .Build();
-            var topicName = String.IsNullOrEmpty(topic) ? message.GetType().Name : topic;
+            var topicName = string.IsNullOrEmpty(topic) ? message.GetType().Name : topic;
             var deliveryResult = await producer.ProduceAsync(topicName, new Message<TKey, TValue>() { Key = key, Value = message });
             producer.Flush();
             producer?.Dispose();
--- Source/KafkaProducer.ConsoleApp/KafkaProducer.ConsoleApp/Program.cs
+++ Source/KafkaProducer.ConsoleApp/KafkaProducer.ConsoleApp/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace KafkaProducer.ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            await PublishMessages();
+        }
+
+        private static async Task PublishMessages()
+        {
+            // create producer
+            Dictionary<string, string> properties = new Dictionary<string, string>();
+            properties.Add("bootstrap.servers", "localhost:9092");
+            Producer producer = new Producer(properties);
+
+            Console.WriteLine("Enter message to publish -");
+            string userInput = Console.ReadLine();
+            await producer.Publish("user-input", userInput);
+            Console.WriteLine("Published...");
         }
     }
 }
